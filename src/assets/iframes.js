a5fa893d5f143f90041e70ed59ecf259ed143631
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-01 12:17:41 +0330

    feat: add Lottie icons and install lottie-react package

---
+++ src/assets/iframes.js
@@ -0,0 +1,15 @@
+export const iframeUrls = {
+  animation1: "https://lottie.host/embed/5445eff9-aa04-49ce-a907-66d6495d4225/SqMMYzbbyY.json",
+  animation2: "https://lottie.host/embed/fa00a4a2-b470-4f92-abbc-167e437649b6/BR1QQJLqSF.json",
+  animation3: "https://lottie.host/embed/a3b9519f-8cee-4905-8dd6-7970b91c5b5c/LHi65x1kYc.json",
+  animation4: "https://lottie.host/embed/ac8545ae-6135-451e-9b3f-80541e386cbd/SpFLAvJqLn.json",
+};
+
+
+//* uesed icon animtions 
+{/* <iframe src={iframeUrls.animation1} className="w-24 h-24"></iframe>
+<iframe src={iframeUrls.animation2} className="w-24 h-24"></iframe> */}
+
+// import Lottie from "lottie-react";
+// import animationData from "../assets/icons/Animation - 1727768987571.json";
+// import { iframeUrls } from "../assets/iframes";
