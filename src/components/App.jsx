1a46bbd1067a1ab2cefa7ae1b5908cfe1a404440
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-01 12:22:15 +0330

    refactor: update main and App components for better structure and functionality

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+// import Lottie from "lottie-react";
+// import animationData from "../assets/icons/Animation - 1727768987571.json";
+// import { iframeUrls } from "../assets/iframes";
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>hello everyone</h1>
+    </div>
+  );
+}
