fe3c227f8b4ea60162b3cd03591555a8c6999e07
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-01 19:44:38 +0330

    Created SearchDashboard component and designed its UI

--- src/components/SearchDashboard.jsx
+++ src/components/SearchDashboard.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { IoSearch } from "react-icons/io5";
 
-export default function SearchDashboard() {
+export default function SearchForm() {
   return (
-    <div>SearchDashboard</div>
-  )
+    <form className="flex items-center">
+      <div className="relative w-1/2">
+        <input
+          type="text"
+          className="bg-[#0E1421] py-4 px-7 w-full rounded-[20px] pr-10 text-[#676B73]"
+          placeholder="enter your city name..."
+        />
+        <button>
+          <IoSearch className="absolute right-5 top-3 text-[#742BEC] w-7 h-7" />
+        </button>
+      </div>
+    </form>
+  );
 }
