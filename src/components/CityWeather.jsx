3108d9dcc81f1c4f8394316a5ab28919a7f5176f
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-01 12:20:03 +0330

    feat: create components for weather dashboard, including CityWeather, SearchDashboard,WeatherMap,WeekWeatherSummary, and TodayHighlight

---
+++ src/components/CityWeather.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CityWeather() {
+  return (
+    <div>CityWeather</div>
+  )
+}
---
+++ src/components/WeekWeatherSummary.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WeekWeatherSummary() {
+  return (
+    <div>WeekWeatherSummary</div>
+  )
+}
