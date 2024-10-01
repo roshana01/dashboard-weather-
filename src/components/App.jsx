cb0c9b20e42cd13bc9c1497536def90feec3365e
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-01 19:47:19 +0330

    Added LogoTitle, SearchDashboard, and CityWeather components to the app layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,20 @@
 // import { iframeUrls } from "../assets/iframes";
 
 import React from "react";
+import SideBar from "./SideBar/SideBar";
+import SearchDashboard from "./SearchDashboard";
+import CityWeather from "./CityWeather";
 
 export default function App() {
   return (
-    <div>
-      <h1>hello everyone</h1>
+    <div className="flex h-screen">
+      <SideBar />
+      <div className="flex-1 p-2 ">
+        <SearchDashboard />
+        <div className="mt-4 w-[690px] h-[369px] bg-[#0E1421]  rounded-[20px] ">
+          <CityWeather />
+        </div>
+      </div>
     </div>
   );
 }
