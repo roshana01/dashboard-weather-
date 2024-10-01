98a7e70f1a7d850cf6346f9edc38ebe3a0c96368
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-01 19:40:17 +0330

    Create data file for icons

---
+++ src/data/dataIcons.js
@@ -0,0 +1,27 @@
+export const iconData = [
+  {
+    id: 1,
+    name: 'icon1',
+    src: './src/assets/img/Vector2.svg',
+  },
+  {
+    id: 2,
+    name: 'icon2',
+    src: './src/assets/img/chart2.svg',
+  },
+  {
+    id: 3,
+    name: 'icon3',
+    src: './src/assets/img/location.svg',
+  },
+  {
+    id: 4,
+    name: 'icon3',
+    src: './src/assets/img/calender.svg',
+  },
+  {
+    id: 5,
+    name: 'icon4',
+    src: './src/assets/img/setting.svg',
+  },
+];
