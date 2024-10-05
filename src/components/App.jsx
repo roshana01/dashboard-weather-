e51e6ded4be2e0e777fa6acd822c7f3d522162bb
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-05 10:07:39 +0330

    feat: integrate useContext for global state management

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,23 @@
-// import Lottie from "lottie-react";
-// import animationData from "../assets/icons/Animation - 1727768987571.json";
-// import { iframeUrls } from "../assets/iframes";
-
 import React from "react";
 import SideBar from "./SideBar/SideBar";
 import SearchDashboard from "./SearchDashboard";
-import CityWeather from "./CityWeather";
+import { WeatherProvider } from "../context/WeatherContext ";
+import WeatherOverview from "./WeatherSection/WeatherOverview";
+import TodayHighlights from "./TodayHighlights";
 
 export default function App() {
   return (
-    <div className="flex h-screen">
-      <SideBar />
-      <div className="flex-1 p-2 ">
-        <SearchDashboard />
-        <div className="mt-4 w-[690px] h-[369px] bg-[#0E1421]  rounded-[20px] ">
-          <CityWeather />
+    <WeatherProvider>
+      <div className="flex h-screen">
+        <SideBar />
+        <div className="flex-1 p-2 ">
+          <SearchDashboard />
+          <div className="mt-4 w-[690px] h-[369px] bg-[#0E1421] rounded-[20px] ">
+            <WeatherOverview />
+          </div>
         </div>
+        {/* <TodayHighlights /> */}
       </div>
-    </div>
+    </WeatherProvider>
   );
 }
