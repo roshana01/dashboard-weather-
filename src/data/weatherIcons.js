711d9683fa0685b277954b0cfd59e2fe7a3ca3d4
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-05 10:04:12 +0330

    Organized icons based on conditions like 'clear sky', 'few clouds', 'rain', 'snow', and more.

---
+++ src/data/weatherIcons.js
@@ -0,0 +1,36 @@
+export const weatherIcons = {
+  "clear sky": () => import("../assets/icons/Animation - 1727797538033.json"), //sun
+  // clouds
+  "few clouds": () => import("../assets/icons/Animation - 1727789626500.json"), //sun and clouds
+  "scattered clouds": () => import("../assets/icons/Animation - 1727798066622.json"), //
+  "overcast clouds": () => import("../assets/icons/Animation - 1727797986355.json"),
+  "broken clouds": () => import("../assets/icons/Animation - 1727798066622.json"),
+  // haze
+  haze: () => import("../assets/icons/Animation - 1727946674353.json"),
+  mist: () => import("../assets/icons/Animation - 1727954704220.json"),
+  drizzle: () => import("../assets/icons/Animation - 1727946674353.json"),
+  "light intensity drizzle": () => import("../assets/icons/Animation - 1727946674353.json"),
+
+  // rain
+  rain: () => import("../assets/icons/Animation - 1727798170130.json"),
+  "light intensity shower rain": () => import("../assets/icons/Animation - 1727797667483.json"),
+  "heavy intensity rain": () => import("../assets/icons/Animation - 1727797667483.json"),
+  "light rain": () => ("../assets/icons/Animation - 1727958706876.json"),
+  "moderate rain": () => import("../assets/icons/Animation - 1727956675462.json"),
+  "ragged shower rain": () => import("../assets/icons/Animation - 1727956675462.json"),
+  thunderstorm: () => import("../assets/icons/Animation - 1727797538033.json"),
+  //	 snow
+  snow: () => import("../assets/icons/Animation - 1727797667483.json"),
+  "light snow": () => import("../assets/icons/Animation - 1727798246555.json"),
+  "light snow": () => import("../assets/icons/Animation - 1727798246555.json"),
+  "light shower sleet": () => import("../assets/icons/Animation - 1727798246555.json"),
+  "light shower snow": () => import("../assets/icons/Animation - 1727798246555.json"),
+  "heavy snow": () => import("../assets/icons/Animation - 1727797959870.json"),
+  "heavy shower snow": () => import("../assets/icons/Animation - 1727797959870.json"),
+
+}
+
+
+
+
+// ../assets/icons/Animation - 1727946674353.json
