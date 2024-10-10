a0ede0456ebb785ab5ccdceb1d49dd47c12b96a9
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-10 10:30:19 +0330

    feat: add and update WeatherOverview component with responsive design

--- src/components/WeatherSection/WeatherOverview.jsx
+++ src/components/WeatherSection/WeatherOverview.jsx
@@ -9,17 +9,17 @@ import InvalidCityError from "./InvalidCityError";
 // [few clouds][ clear sky] [overcast clouds] [scattered clouds] [broken clouds][ haze ,rain , snow , sun]
 
 export default function WeatherOverview() {
-  const { weatherData, error } = useContext(WeatherContext);
+  const { weatherData, WeatherIsLoading } = useContext(WeatherContext);
 
   return (
-    <>
-      {!weatherData && !error ? (
+    <div className="mt-4 w-full sm:w-[690px] h-[270px] sm:h-[350px] bg-[#0E1421] rounded-[20px]">
+      {!weatherData && !WeatherIsLoading ? (
         <ExploreTheWeather />
-      ) : error ? (
+      ) : WeatherIsLoading ? (
         <InvalidCityError />
       ) : (
         <WeatherDetails />
       )}
-    </>
+    </div>
   );
 }
