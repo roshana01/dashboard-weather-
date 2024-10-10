94466c6862b62c3dc0a65123a58b9531a4146355
roshanak roshani <roshanaroshani99@gmail.com>
2024-10-10 10:31:08 +0330

    feat: add and update WeatherContext for state management improvements

--- src/context/WeatherContext .jsx
+++ src/context/WeatherContext .jsx
@@ -1,16 +1,48 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 export const WeatherContext = createContext(); //1
 import { debounce } from "lodash";
 import { useForm } from "react-hook-form";
 import { useWeatherApi } from "../hook/useWeatherApi";
 import { format } from "date-fns";
+import useWeatherForecast from "../hook/useWeatherForecast";
+import moment from "moment";
+import { useCapitalWeatherApi } from "../hook/useCapitalWeatherData";
 
 export const WeatherProvider = ({ children }) => {
-  const [cityName, setCityName] = useState("Vienna");
+  //state
+  const [cityName, setCityName] = useState("Russia");
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const { data: weatherData, error, isLoading } = useWeatherApi(cityName);
-  const { register, handleSubmit, reset } = useForm();
+  const [animationData, setAnimationData] = useState("");
+  const [coord, setCoord] = useState({});
+  const [forecastDataForWeek, setForecastDataForWeek] = useState([]);
+  const [tomorrowData, setTomorrowData] = useState(null);
 
+  //query
+  const {
+    data: weatherData,
+    error: weatherError,
+    isLoading: WeatherIsLoading,
+  } = useWeatherApi(cityName);
+  const {
+    data: weekWeatherData,
+    error: weekWeatherError,
+    isLoading: weekWeatherLoading,
+  } = useWeatherForecast(coord);
+  // get coord lat lon getWeatherForecast:
+  const {
+    data: capitalWeatherData,
+    error: capitalWeatherError,
+    isLoading: capitalWeatherLoading,
+  } = useCapitalWeatherApi();
+  
+
+  useEffect(() => {
+    if (weatherData?.coord) setCoord(weatherData.coord);
+  }, [weatherData?.coord]);
+
+  //Hook-form
+  const { register, handleSubmit, reset } = useForm();
+  //data uesWeatherApi
   const city = weatherData?.name;
   const temperature = weatherData?.main.temp.toFixed(0);
   const tempMin = weatherData?.main?.temp_min.toFixed(0);
@@ -23,10 +55,20 @@ export const WeatherProvider = ({ children }) => {
   const currentDate = format(new Date(), "eeee");
   const formattedDate = format(new Date(), "dd MMMM, yyyy");
 
-  <div className="text-white">
-    <h2 className="text-[40px] mt-[34px] font-medium">{currentDate}</h2>
-    <p className="mt-2 text-[16px]">{formattedDate}</p>
-  </div>;
+  // data uesWeatherForecast:
+
+  useEffect(() => {
+    if (weekWeatherData && weekWeatherData.list) {
+      setForecastDataForWeek(weekWeatherData.list.slice(0, 7));
+
+      // Find tomorrow's data
+      const tomorrow = moment().add(1, "days").format("YYYY-MM-DD");
+      const tomorrowWeather = weekWeatherData.list.find(
+        (item) => moment(item.dt_txt).format("YYYY-MM-DD") === tomorrow
+      );
+      setTomorrowData(tomorrowWeather);
+    }
+  }, [weekWeatherData]);
 
   const debouncedSearch = useCallback(
     debounce((value) => {
@@ -54,14 +96,16 @@ export const WeatherProvider = ({ children }) => {
       <WeatherContext.Provider
         value={{
           weatherData,
-          isLoading,
-          error,
+          weatherError,
           cityName,
           setCityName,
           isSidebarOpen,
           setIsSidebarOpen,
+          animationData,
+          setAnimationData,
+          coord,
           toggleSidebar,
-          isLoading,
+          WeatherIsLoading,
           register,
           handleSubmit,
           handleCityChange,
@@ -76,6 +120,14 @@ export const WeatherProvider = ({ children }) => {
           currentDate,
           formattedDate,
           descriptionWeatherIcons,
+          weekWeatherData,
+          weekWeatherError,
+          weekWeatherLoading,
+          forecastDataForWeek,
+          tomorrowData,
+          capitalWeatherData,
+          capitalWeatherError,
+          capitalWeatherLoading
         }}
       >
         {children}
